b029836a8497ebc4c8fcac3a5af01d8e79f6f317
LUIS <luisfer811@gmail.com>
2025-01-20 15:27:57 -0500

    Implementación de una función para sortear los amigos

--- app.js
+++ app.js
@@ -1,30 +1,41 @@
 let amigos = []
 let regex = /^[a-zA-Z\s]+$/;
-let lista = document.getElementById("listaAmigos")
+let lista = document.getElementById("listaAmigos");
+let resultado = document.getElementById("resultado");
 
 
 let agregarAmigo=()=>{
-  let nombre = document.getElementById("amigo").value
+  let nombre = document.getElementById("amigo").value;
    if(regex.test(nombre)){
     amigos.push(nombre);
     console.log(amigos);    
-    document.getElementById("amigo").value=""
+    document.getElementById("amigo").value="";
   }else{
-    alert ("Por favor, inserte un nombre.")
+    alert ("Por favor, inserte un nombre.");
   }
-  return actualizarAmigos()
+  return actualizarAmigos();
 }
 
 
 let actualizarAmigos=()=>{
   
-  lista.innerHTML=""
+  lista.innerHTML="";
   for(i=0; i<amigos.length; i++){
     let nuevoAmigo = document.createElement("li");
     nuevoAmigo.textContent = amigos[i];
     lista.appendChild(nuevoAmigo);
   }
-
+ return;
 }
+
+let sortearAmigo=()=>{
+  if(amigos.length!==0){
+    let indiceAleatorio = Math.floor(Math.random()*amigos.length);
+    console.log(indiceAleatorio);
+    let nombreSorteado = amigos[indiceAleatorio];
+    resultado.innerHTML = nombreSorteado;
+  }
+}
+
 
 
